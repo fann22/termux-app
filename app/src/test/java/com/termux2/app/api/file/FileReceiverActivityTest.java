9e122b4e4075ed66188cfcadb87418a8c1527a05
Fann22 <rilisan5@gmail.com>
2025-12-07 09:13:25 +0700

    Last?

--- app/src/test/java/com/termux2/app/api/file/FileReceiverActivityTest.java
+++ app/src/test/java/com/termux2/app/api/file/FileReceiverActivityTest.java
@@ -1,6 +1,6 @@
 package com.termux2.app.api.file;
 
-import com.termux.app.api.file.FileReceiverActivity;
+import com.termux2.app.api.file.FileReceiverActivity;
 
 import org.junit.Assert;
 import org.junit.Test;
